91d4abdf2f4c7e57ccdb57a765e97ff54bfc2996
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-04-14 23:06:09 +0800

    Add layers

---
+++ spacemacs/README.md
@@ -0,0 +1,5 @@
+Ignore `spacemacs` (`.spacemacs`) file because it will just change regularly. I
+keep it here just for reference.
+
+Put the layers in `~/.emacs.d/private/` and add their names in `.spacemacs`
+under `dotspacemacs-configuration-layers`.
---
+++ spacemacs/layers/paredit/packages.el
@@ -0,0 +1,31 @@
+;;; packages.el --- paredit layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Burhanuddin <burhanuddin@burhanuddin-Aspire-V5-473PG>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defconst paredit-packages '(paredit))
+
+(defun paredit/init-paredit ()
+  (use-package paredit
+    :defer t
+    :init
+    (progn
+      (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+      (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+      (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+      (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+      (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+      (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+      (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+      (add-hook 'slime-repl-mode-hook
+                (lambda ()
+                  (define-key slime-repl-mode-map
+                    (read-kbd-macro paredit-backward-delete-key) nil))))))
+
+;;; packages.el ends here
